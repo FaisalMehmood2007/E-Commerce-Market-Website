ee4febb1f98e3df12da6ff2176e3c20c70e79985
FaisalMehmood2007 <faisalbhagri2007@gmail.com>
2023-12-29 15:04:52 +0500

    added conversation app to the project

--- firstcrm/settings.py
+++ firstcrm/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'core',
     'dashboard',
     'item',
+    'conversation',
 ]
 
 MIDDLEWARE = [
--- firstcrm/urls.py
+++ firstcrm/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('', include('core.urls')),
     path('items/', include('item.urls')),
     path('dashboard/', include('dashboard.urls')),
+    path('inbox/', include('conversation.urls')),
     # path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
